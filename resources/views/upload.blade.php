2e55ed4304f834c8d9f96f60208dbef16aa96043
ekzor <ekzor@users.noreply.github.com>
2017-09-07 18:21:41 -0400

    added progress bars for individual file uploads
    Addresses issue #11

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -20,11 +20,28 @@
             direction: ltr;
             cursor: pointer;
         }
-        .upload-Success {
+        .upload_status {
+            width:100px;
+        }
+        .upload_status-Success {
             color: #2b542c;
         }
-        .upload-UploadError {
+        .upload_status-UploadError {
             color: #411c0e;
+        }
+        #files > tbody > tr > td.upload_table_progress_container {
+            line-height: 0;
+            border: none;
+            padding-top: 0px;
+            padding-bottom: 0px;
+        }
+        .upload_table_progress_row {
+            height: 4px;
+        }
+        .upload_table_progress {
+            height: 2px;
+            line-height:2px;
+            font-size:2px;
         }
         .screenshot {
             margin-top: 22px;
@@ -44,7 +61,7 @@
     <hr>
 
     <h1>Or you can use the web uploader</h1>
-    <p>Choose replay files, or drag them onto this page. <span id="container_fileupload_dir_text">Chrome users: If you are uploading &gt;700 replays, please select by directory</span></p>
+    <p>Choose replay files, or drag them onto this page. <span id="container_fileupload_dir_text hidden">Chrome users: If you are uploading &gt;700 replays, please select by directory</span></p>
 
     <span class="btn btn-success fileinput-button">
         <i class="glyphicon glyphicon-plus"></i>
@@ -52,7 +69,7 @@
         <input id="fileupload" type="file" name="file" multiple>
     </span>
 
-    <span class="btn btn-success fileinput-button" id="container_fileupload_dir">
+    <span class="btn btn-success fileinput-button hidden" id="container_fileupload_dir">
         <i class="glyphicon glyphicon-plus"></i>
         <span>Select directory...</span>
         <input id="fileupload_dir" type="file" name="file" multiple directory webkitdirectory>
@@ -73,28 +90,54 @@
                 url: '/api/v1/replays',
                 sequentialUploads: true,
                 dataType: 'json',
+                
+                start: function(e) {
+                    $('#progress').removeClass('hidden');
+                },
+             
                 drop: function (e, data) {
-                    if ($(this).attr('id') == "fileupload_dir") {
+                    
+                    //stop the drag and drop event for the second upload button to avoid triggering it twice
+                    if ($(this).attr('id') == 'fileupload_dir') {
                         e.preventDefault();
                         return false;
                     }
+                    
                 },
+                
                 done: function (e, data) {
+                    
                     let status = data.result.status ? data.result.status : "UploadError";
-                    $("#files").find('tbody')
-                        .prepend($('<tr>')
-                            .append($('<td>')
-                                .text(data.result.originalName)
-                            ).append($('<td>')
-                                .text(status)
-                                .addClass("upload-" + status)
-                            )
-                        );
+                    
+                    //update status text and add an appropriate class. querySelector stops after the first element is found and should therefore be a little faster than jquery as the table gets larger
+                    $(document.querySelector('.upload_status')).addClass('upload_status-'+status).text(status);
+                    
+                    //remove individual file progressbar (keep the empty row to avoid having the table contents jump around)
+                    $('.upload_table_progress').remove();
+                    
                 },
-                progressall: function (e, data) {
+                
+                send: function(e, data) {
+                    
+                    //add the file to the table once the transfer begins and include a second row for its progress indicator
+                    $('#files > tbody').prepend('<tr><td class=upload_filename>'+data.files[0].name+'</td><td class=upload_status>&nbsp;</td></tr><tr class=upload_table_progress_row><td colspan=2 class=upload_table_progress_container><div class="progress-bar upload_table_progress">&nbsp;</div></td></tr>');
+               
+               },
+               
+                progress: function(e, data) {
+                    
+                    //update the individual file progress indicator
                     let progress = parseInt(data.loaded / data.total * 100, 10);
-                    $('#progress').removeClass('hidden');
+                    $('#files').find('.progress-bar').css('width', progress + '%');
+                    
+                },
+                
+                progressall: function (e, data) {
+                    
+                    //update the overall progress indicator
+                    let progress = parseInt(data.loaded / data.total * 100, 10);
                     $('#progress .progress-bar').css('width', progress + '%');
+                    
                 }
             });
         });
@@ -109,9 +152,7 @@
         }
         
         if (isInputDirSupported()) {
-            $("#container_fileupload_dir_text,#container_fileupload_dir").show()
-        } else {
-            $("#container_fileupload_dir_text,#container_fileupload_dir").hide()
-        }
+            $("#container_fileupload_dir_text,#container_fileupload_dir").removeClass('hidden');
+        } 
     </script>
 @endsection
