00d3daf6c5b2ffd2e8c1b4ef630612fb2ef3edc6
Roman Semenov <Semenov.Roman@mail.ru>
2017-09-10 10:32:01 +0300

    update config defaults

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'HotsApi'),
 
     /*
     |--------------------------------------------------------------------------
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://hotsapi.net'),
 
     /*
     |--------------------------------------------------------------------------
