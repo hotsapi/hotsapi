6b4322ca42e88e3ca6cfa7c23f16c72150bfc552
Roman Semenov <Semenov.Roman@mail.ru>
2017-09-14 13:13:23 +0300

    guids are 36 symbols long

--- database/migrations/2017_08_16_061032_create_replays_table.php
+++ database/migrations/2017_08_16_061032_create_replays_table.php
@@ -16,14 +16,14 @@ class CreateReplaysTable extends Migration
         Schema::create('replays', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->string('filename', 37)->unique();
+            $table->string('filename', 36)->unique();
             $table->integer('size');
             $table->string('game_type', 32)->nullable()->index();
             $table->dateTime('game_date')->nullable()->index();
             $table->integer('game_length')->nullable();
             $table->string('game_map', 32)->nullable()->index();
             $table->string('game_version', 32)->nullable();
-            $table->string('fingerprint', 37)->unique();
+            $table->string('fingerprint', 36)->unique();
         });
     }
 
