94dc0d48b98e97fde0121d9db8dd1a86a60df012
Roman Semenov <Semenov.Roman@mail.ru>
2017-10-04 13:42:22 +0300

    remove thebutcher special case

--- app/Console/Commands/FetchTranslations.php
+++ app/Console/Commands/FetchTranslations.php
@@ -71,7 +71,7 @@ class FetchTranslations extends Command
         $json = json_decode(\Guzzle::get('https://api.hotslogs.com/Public/Data/Heroes')->getBody());
         foreach ($json as $hero) {
             $dbHero = $heroes->where('name', $hero->PrimaryName)->first();
-            $shortName = preg_replace('/thebutcher/', 'butcher', strtolower(preg_replace('/[^\w]/', '', iconv('UTF-8', 'ASCII//TRANSLIT', $hero->PrimaryName))));
+            $shortName = strtolower(preg_replace('/[^\w]/', '', iconv('UTF-8', 'ASCII//TRANSLIT', $hero->PrimaryName)));
             if (!$dbHero) {
                 $dbHero = Hero::create(['name' => $hero->PrimaryName, 'short_name' => $shortName]);
             }
