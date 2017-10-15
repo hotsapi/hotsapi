259aaeaf6e39a1f67e715690a84a2be88b53305b
Roman Semenov <Semenov.Roman@mail.ru>
2017-10-15 12:12:57 +0300

    optimize scores table

--- database/migrations/2017_09_29_151003_create_scores_table.php
+++ database/migrations/2017_09_29_151003_create_scores_table.php
@@ -14,32 +14,31 @@ class CreateScoresTable extends Migration
     public function up()
     {
         Schema::create('scores', function (Blueprint $table) {
-            $table->integer('id')->unsigned()->unique();
-            $table->primary('id');
+            $table->integer('id')->unsigned()->primary();
             $table->foreign('id')->references('id')->on('players')->onDelete('cascade');
 
-            $table->integer('level')->nullable();
-            $table->integer('kills')->nullable();
-            $table->integer('assists')->nullable();
-            $table->integer('takedowns')->nullable();
-            $table->integer('deaths')->nullable();
-            $table->integer('highest_kill_streak')->nullable();
-            $table->integer('hero_damage')->nullable();
-            $table->integer('siege_damage')->nullable();
-            $table->integer('structure_damage')->nullable();
-            $table->integer('minion_damage')->nullable();
-            $table->integer('creep_damage')->nullable();
-            $table->integer('summon_damage')->nullable();
-            $table->integer('time_cc_enemy_heroes')->nullable();
-            $table->integer('healing')->nullable();
-            $table->integer('self_healing')->nullable();
-            $table->integer('damage_taken')->nullable();
-            $table->integer('experience_contribution')->nullable();
-            $table->integer('town_kills')->nullable();
-            $table->integer('time_spent_dead')->nullable();
-            $table->integer('merc_camp_captures')->nullable();
-            $table->integer('watch_tower_captures')->nullable();
-            $table->integer('meta_experience')->nullable();
+            $table->tinyInteger('level')->unsigned()->nullable();
+            $table->tinyInteger('kills')->unsigned()->nullable();
+            $table->tinyInteger('assists')->unsigned()->nullable();
+            $table->tinyInteger('takedowns')->unsigned()->nullable();
+            $table->tinyInteger('deaths')->unsigned()->nullable();
+            $table->tinyInteger('highest_kill_streak')->unsigned()->nullable();
+            $table->integer('hero_damage')->unsigned()->nullable();
+            $table->integer('siege_damage')->unsigned()->nullable();
+            $table->integer('structure_damage')->unsigned()->nullable();
+            $table->integer('minion_damage')->unsigned()->nullable();
+            $table->integer('creep_damage')->unsigned()->nullable();
+            $table->integer('summon_damage')->unsigned()->nullable();
+            $table->integer('time_cc_enemy_heroes')->unsigned()->nullable();
+            $table->integer('healing')->unsigned()->nullable();
+            $table->integer('self_healing')->unsigned()->nullable();
+            $table->integer('damage_taken')->unsigned()->nullable();
+            $table->integer('experience_contribution')->unsigned()->nullable();
+            $table->tinyInteger('town_kills')->unsigned()->nullable();
+            $table->smallInteger('time_spent_dead')->unsigned()->nullable();
+            $table->tinyInteger('merc_camp_captures')->unsigned()->nullable();
+            $table->tinyInteger('watch_tower_captures')->unsigned()->nullable();
+            $table->integer('meta_experience')->unsigned()->nullable();
         });
     }
 
