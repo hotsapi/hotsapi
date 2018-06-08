3d568d7fb5c2c65bb2775d01f5dace28b862211d
MGatner <mgatner@icloud.com>
2018-06-07 20:57:33 -0400

    add Heroes Share logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,6 +40,7 @@
             <a href="https://hotstat.us" class="partner-link"><img src="{{asset('/img/logo-hotstatus.png')}}"></a>
             <a href="https://play.google.com/store/apps/details?id=com.heroescompanion.app" class="partner-link"><img src="{{asset('/img/logo-heroescompanion.png')}}"></a>
 			<a href="https://hots.academy" class="partner-link"><img src="{{asset('/img/logo-hotsacademy.png')}}"></a>
+			<a href="https://heroesshare.net" class="partner-link"><img src="{{asset('/img/logo-heroesshare.png')}}"></a>
         </div>
     </div>
 
