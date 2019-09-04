f6529bb73b499fa444287d622468e0643348fd26
poma <Semenov.Roman@mail.ru>
2019-09-04 05:11:18 +0300

    change some defaults to https

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://hotsapi.net'),
+    'url' => env('APP_URL', 'https://hotsapi.net'),
 
     /*
     |--------------------------------------------------------------------------
